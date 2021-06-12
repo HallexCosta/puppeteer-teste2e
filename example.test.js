cadad4485867a6c17e2a17103a6d52fe7852d9e8
HallexCosta <hallex.costa@hotmail.com>
2021-06-12 02:24:04 -0300

    chore: fix test

--- example.test.js
+++ example.test.js
@@ -1,23 +1,25 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer')
 // tests e2e
 
 class Example {
   async start() {
     const browser = await puppeteer.launch({
-      args: ["--no-sandbox"],
+      args: ['--no-sandbox'],
       executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
-      headless: false,
-    });
-    const page = await browser.newPage();
-    await page.goto("https://example.com");
-    console.log(await page.title());
+      headless: false
+    })
+    const page = await browser.newPage()
+    await page.goto('https://example.com')
+    console.log(await page.title())
 
-    await browser.close();
+    await browser.close()
   }
 }
 
 // Should be able get title "Example Domain"
 {
-  const example = new Example();
-  await example.start();
+  ;(async () => {
+    const example = new Example()
+    await example.start()
+  })()
 }
