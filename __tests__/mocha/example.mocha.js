89ff088dd970724c2d722b09cf2ffe86ae1d1966
HallexCosta <hallex.costa@hotmail.com>
2021-06-15 14:41:04 -0300

    chore: add more args on mocha test js

--- __tests__/mocha/example.mocha.js
+++ __tests__/mocha/example.mocha.js
@@ -4,16 +4,20 @@ const puppeteer = require('puppeteer')
 class Example {
   async start() {
     const browser = await puppeteer.launch({
-      args: ['--no-sandbox'],
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-infobars',
+        '--window-position=0,0',
+        '--ignore-certifcate-errors',
+        '--ignore-certifcate-errors-spki-list',
+        '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'
+      ],
       executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
-      headless: true
+      headless: true,
+      ignoreHTTPSErrors: true
     })
     const page = await browser.newPage()
-
-    const userAgent =
-      'Mozilla/5.0 (X11; Linux x86_64)' +
-      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36'
-    await page.setUserAgent(userAgent)
 
     const uri = `https://yayanimes.net/lista-de-animes`
 
