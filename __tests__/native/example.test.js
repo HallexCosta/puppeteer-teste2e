2b9da547a976fbaaa6a73ca10c9065091f9955e2
HallexCosta <hallex.costa@hotmail.com>
2021-06-15 15:07:22 -0300

    chore: add new args in native js

--- __tests__/native/example.test.js
+++ __tests__/native/example.test.js
@@ -5,9 +5,18 @@ const puppeteer = require('puppeteer')
 class Example {
   async start() {
     const browser = await puppeteer.launch({
-      args: ['--no-sandbox'],
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-infobars',
+        '--window-position=0,0',
+        '--ignore-certifcate-errors',
+        '--ignore-certifcate-errors-spki-list',
+        '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'
+      ],
       executablePath: process.env.PUPPETEER_EXEC,
-      headless: true
+      headless: true,
+      ignoreHTTPSErrors: true
     })
     const page = await browser.newPage()
 
