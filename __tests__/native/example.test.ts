46af3a0ec2613f1248e0e699cbf82da8d696c294
HallexCosta <hallex.costa@hotmail.com>
2021-06-15 13:18:29 -0300

    feat: add native test in typescript

---
+++ __tests__/native/example.test.ts
@@ -0,0 +1,55 @@
+// tests e2e
+import { deepStrictEqual } from 'assert'
+import * as puppeteer from 'puppeteer'
+
+class Example {
+  public title: string = ''
+  public names: string[] = []
+
+  async start() {
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox'],
+      executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
+      headless: true
+    })
+    const page = await browser.newPage()
+
+    const uri = `https://yayanimes.net/lista-de-animes`
+
+    await page.goto(uri, {
+      timeout: 0,
+      waitUntil: 'networkidle2'
+    })
+
+    this.title = await page.title()
+
+    this.names = await page.evaluate(() => {
+      const anchors = [...document.querySelectorAll(' .aba ul > li > a')]
+
+      return anchors.map(anchor => String(anchor.textContent))
+    })
+
+    await browser.close()
+  }
+}
+
+const example = new Example()
+
+// Should be able get title "Example Domain"
+{
+  ;(async () => {
+    await example.start()
+
+    deepStrictEqual(example.title, 'Assistir Lista de Animes - Online em FHD')
+  })()
+}
+
+// Should be able get animes names
+{
+  ;(async () => {
+    await example.start()
+
+    deepStrictEqual(typeof example.names, 'object')
+    deepStrictEqual(example.names.length > 0, true)
+  })()
+}
